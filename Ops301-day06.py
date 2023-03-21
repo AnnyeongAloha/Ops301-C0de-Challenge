c1abfa603d70acc87099387f7383f26b6f339e8b
AnnyeongAloha <116544291+AnnyeongAloha@users.noreply.github.com>
2023-03-20 20:10:57 -0700

    Referred to Mola and Demo Script
    Will continue to work on the script to make my own

---
+++ Ops301-day06.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Script: Ops301d6 Challenge-06
+# Author: Justin 'Sage' Tabios
+
+# Requirements:
+# The Python module “os” must be utilized
+# At least three variables must be declared in Python that contain results of bash operations
+# The Python function print() must be used at least three times
+# Include execution of the following bash commands inside your Python script:
+
+# whoami
+# cd $Home
+# lshw -short
+
+
+
+
+# How to use Linux/Bash commands within Python
+# The Python module “os” must be utilized
+import os
+
+# Then use os.system to call any kind of bash command
+# At least three variables must be declared in Python that contain results of bash operations
+var1 = [os.system("whoami")]
+var2 = [os.system("cd $Home")]
+var3 = [os.system("lshw -short")]
+
+
+# The Python function print() must be used at least three times
+print (var1)
+print (var2)
+print (var3)
