8d079bc9fa287c33423b259ebb74a6eb305fd4c7
AnnyeongAloha <116544291+AnnyeongAloha@users.noreply.github.com>
2023-03-23 12:32:36 -1000

    Complete

---
+++ Ops301-day09.py
@@ -0,0 +1,32 @@
+# Ops301 Code Challenge day09 
+
+# Author: Justin 'Sage' Tabios
+
+# Date of revision: 23 Mar 2023
+
+# Purpose: Create a python script that uses if statements using logical conditions
+
+#!/usr/bin/env python3
+
+# Define variables 
+
+a = 66 
+b = 666 
+
+a == b 
+a != b
+a < b
+a <= b
+a > b 
+a >= b 
+
+# Create if statements and print information to the screen depending on conditions met
+
+if a == b:
+    print(a and b are equal)
+    elif a > b 
+    print(a is greater than b)
+    elif a != b
+    print(a is not b, Son!)
+else:
+    print("GTFO!")
